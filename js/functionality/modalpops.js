70161d5cdfe1983255c90035575227ccbaa264b8
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-06-06 12:23:32 +0200

    fix active links when modals open/close

--- js/functionality/modalpops.js
+++ js/functionality/modalpops.js
@@ -1,8 +1,14 @@
 export function openModal(navigation, body, overlay, modal) {
   //hide navigation on mobile when modals visible
+  //remove active class from home link
   const browserWidth = window.innerWidth;
+  let homelink = document.querySelector("#home-mobile");
   if (browserWidth <= 650) {
     navigation.style.transform = "translateY(-150%)";
+    homelink.classList.remove("active");
+  } else if (browserWidth > 650) {
+    homelink = document.querySelector("#home-desktop");
+    homelink.classList.remove("active");
   }
 
   //apply modal styles
@@ -13,9 +19,15 @@ export function openModal(navigation, body, overlay, modal) {
 
 export function closeModal(navigation, body, overlay, modal) {
   //display navigation on mobile again
+  //remove active class from home link
   const browserWidth = window.innerWidth;
+  let homelink = document.querySelector("#home-mobile");
   if (browserWidth <= 650) {
     navigation.style.transform = "translateY(0)";
+    homelink.classList.add("active");
+  } else if (browserWidth > 650) {
+    homelink = document.querySelector("#home-desktop");
+    homelink.classList.add("active");
   }
 
   //remove modal styles
--- js/script.js
+++ js/script.js
@@ -32,17 +32,34 @@ socials.addEventListener("click", () => {
   openModal(navBar, body, overlay, modalSocials);
   //in case user clicks on other modal when previous still on display
   contactInfo.classList.remove("modal-popup");
+  //display active link
+  socials.classList.add("active");
+  //remove active link from the other modal
+  if (contact.classList.contains("active")) {
+    contact.classList.remove("active");
+  }
 });
 
 contact.addEventListener("click", () => {
   openModal(navBar, body, overlay, contactInfo);
   //in case user clicks on other modal when previous still on display
   modalSocials.classList.remove("modal-popup");
+  //display active link
+  contact.classList.add("active");
+  //remove active link from the other modal
+  if (socials.classList.contains("active")) {
+    socials.classList.remove("active");
+  }
 });
 
 closeModalsBtns.forEach((button) => {
   let buttonContainer = button.closest("div");
   button.addEventListener("click", () => {
     closeModal(navBar, body, overlay, buttonContainer);
+    if (buttonContainer.classList.contains("socials-box")) {
+      socials.classList.remove("active");
+    } else if (buttonContainer.classList.contains("contact-box")) {
+      contact.classList.remove("active");
+    }
   });
 });
