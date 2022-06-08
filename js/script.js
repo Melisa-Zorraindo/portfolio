4c34d6c1f0cc7b218cbc7d5171d5a0aba285c0ad
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-06-08 12:06:12 +0200

    light theme enabled

--- js/script.js
+++ js/script.js
@@ -14,3 +14,39 @@ openMenuButton.addEventListener("click", () => {
 closeMenuButton.addEventListener("click", () => {
   closeMenu(navBar, closeMenuButton, openMenuButton);
 });
+
+/* ---------- Dark / Light themes ---------- */
+
+const toggleStatesBtn = document.querySelector("#switch-btn");
+const switchOff = document.querySelector(".state-one");
+const switchOn = document.querySelector(".state-two");
+
+function toggleStates() {
+  switchOn.classList.toggle("on");
+  switchOff.classList.toggle("off");
+}
+
+let lightTheme = localStorage.getItem("lightTheme");
+if (lightTheme === "enabled") {
+  enableLightTheme();
+}
+
+function enableLightTheme() {
+  document.body.classList.add("light");
+  localStorage.setItem("lightTheme", "enabled");
+}
+
+function disableLightTheme() {
+  document.body.classList.remove("light");
+  localStorage.removeItem("lightTheme");
+}
+
+toggleStatesBtn.addEventListener("click", toggleStates);
+toggleStatesBtn.addEventListener("click", () => {
+  lightTheme = localStorage.getItem("lightTheme");
+  if (lightTheme !== "enabled") {
+    enableLightTheme();
+  } else {
+    disableLightTheme();
+  }
+});
