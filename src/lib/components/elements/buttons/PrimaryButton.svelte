7a9438f0d4e47c5a99aa767e8962573b89a28aa9
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-06-04 17:41:45 +0200

    Creates button components

---
+++ src/lib/components/elements/buttons/PrimaryButton.svelte
@@ -0,0 +1,4 @@
+<button type="button" class="btn p-0 variant-filled-primary"
+	><i class="bx bxl-github text-3xl ps-1" />
+	<span class="pe-2">GitHub code</span>
+</button>
---
+++ src/lib/components/elements/buttons/SecondaryButton.svelte
@@ -0,0 +1,4 @@
+<button type="button" class="btn variant-ghost-primary p-0"
+	><i class="bx bx-play text-3xl ps-1" />
+	<span class="pe-2">Live demo</span>
+</button>
