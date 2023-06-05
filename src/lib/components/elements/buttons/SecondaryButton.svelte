dc1f9f16cc8be1406de622b6917a178efa60cf77
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-06-05 08:36:30 +0200

    Restyles secondary button

--- src/lib/components/elements/buttons/SecondaryButton.svelte
+++ src/lib/components/elements/buttons/SecondaryButton.svelte
@@ -1,4 +1,10 @@
-<button type="button" class="btn variant-ghost-primary p-0"
-	><i class="bx bx-play text-3xl ps-1" />
-	<span class="pe-2">Live demo</span>
-</button>
+<div
+	class="p-0.5 bg-gradient-to-br from-primary-500 to-tertiary-500 hover:bg-gradient-to-bl hover:from-primary-500 hover:to-primary-500 rounded-lg"
+>
+	<div
+		class="flex items-center justify-start w-full border-0 rounded-md bg-surface-800 hover:bg-primary-500 hover:text-black transition-all duration-75 ease-in"
+	>
+		<i class="bx bx-play text-3xl" />
+		<button type="button" class="btn py-2 ps-0">Live demo</button>
+	</div>
+</div>
